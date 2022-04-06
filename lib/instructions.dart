897d1b5ceb8c200f81e1cd18650c75161aa0ba2d
Monii22 <80085318+Monii22@users.noreply.github.com>
2022-04-06 23:59:00 +0530

    Some modifications

--- lib/instructions.dart
+++ lib/instructions.dart
@@ -10,16 +10,28 @@ class InstructionsPage extends StatefulWidget {
 
 class _InstructionsPageState extends State<InstructionsPage> {
   ScrollController controller=ScrollController();
-  String str='''
-  # __Instructions on how to use the App__
-  ## 1st Tab (Scan/Upload)
-  1) You have the option to select the image of the sudoku or capture a photo
-     
-  2) ..
-  ## 2nd Tab (Fill)
-  
-  
-  ''';
+
+  Widget image(String s)
+  {
+    return GestureDetector(
+      onTap:()=>showImage(context, "$s"),
+      child: Hero(
+        tag:'hero-tag',
+        child: Container(
+
+          height: 250,
+
+          decoration: BoxDecoration(
+              image:DecorationImage(
+                image:AssetImage("$s"),
+                fit:BoxFit.fitHeight,
+              )
+          ),
+
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
 
@@ -40,6 +52,7 @@ class _InstructionsPageState extends State<InstructionsPage> {
             width: MediaQuery.of(context).size.width,
             fit: BoxFit.cover,
           ),
+          /*
           Scrollbar(
             child:
 
@@ -53,9 +66,82 @@ class _InstructionsPageState extends State<InstructionsPage> {
                 ),
               )
             ,
-          ),
+          ),*/
+          ListView(
+            children:[
+              Center(child: Text("Instructions on how to use the App")),
+              Text("1st Tab (Scan/Upload)"),
+              Text("1) Click the camera icon to take the photo of the sudoku puzzle and click the photo icon to upload the photo of the sudoku puzzle"),
+              GestureDetector(
+                onTap:()=>showImage(context, "assets/App_images/1stTab_1.jpeg"),
+                child: Hero(
+                  tag:'hero-tag',
+                  child: Container(
+
+                    height: 250,
+
+                    decoration: BoxDecoration(
+                      image:DecorationImage(
+                        image:AssetImage("assets/App_images/1stTab_1.jpeg"),
+                        fit:BoxFit.fitHeight,
+                      )
+                    ),
+
+                  ),
+                ),
+              ),
+              Text("2) After that crop out only the sudoku puzzle from the image"),
+              Text("Correct cropping:"),
+              image("assets/App_images/Correct_crop_1.jpeg"),
+              Text("Wrong cropping: "),
+              Row(
+                children:[
+                  Expanded(
+                    child: image("assets/App_images/Wrong_crop_1.jpeg")
+                  ),
+                  Expanded(
+                  child:image("assets/App_images/Wrong_crop_2.jpeg")
+                  ),
+                ]
+              ),
+              Text("3) Once the cropped sudoku puzzle is obtained click 'Save' and then 'Ok'"),
+              Row(
+                  children:[
+                    Expanded(
+                      child: image("assets/App_images/Crop_1.jpeg")
+                    ),
+                    Expanded(
+                      child:image("assets/App_images/1stTab_2.jpeg")
+                    ),
+                  ]
+              ),
+              Text("4)The numbers are scanned and the filled sudoku is displayed on the next tab"),
+              Text(" 2nd Tab (Fill)"),
+              Text("1)Necessary changes can be made in the sudoku if the wrong numbers are scanned and displayed"),
+              image("assets/App_images/Fill_1.jpeg"),
+              Text("2)After cross verifying the numbers displayed and the sudoku in the image click the 'Submit' button"),
+              image("assets/App_images/Fill_2.jpeg"),
+              Text("3)The solved sudoku gets dsplayed after a few seconds"),
+              image("assets/App_images/Fill_3.jpeg")
+
+
+
+            ]
+          )
         ],
       )
     );
   }
+}
+void showImage(BuildContext context,String s){
+  Navigator.of(context).push(
+    MaterialPageRoute(builder: (ctx)=>Scaffold(
+      body:Center(
+        child:Hero(
+          tag: 'hero-tag',
+          child: Image.asset("$s",width: 300),
+        )
+      )
+    ))
+  );
 }
